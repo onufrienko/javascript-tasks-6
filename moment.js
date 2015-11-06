259ad16a7bf6a277ff46c120a28124621b5e7169
Onufrienko Ekaterina <kat.onuf@yandex.ru>
2015-11-06 19:37:11 +0500

    Moment without *

--- moment.js
+++ moment.js
@@ -2,7 +2,7 @@
 
 module.exports = function () {
     return {
-        // Здесь как-то хранится дата ;)
+        // Здесь как-то хранится дата ;) 'ПН 16:00'
         date: null,
 
         // А здесь часовой пояс
@@ -10,6 +10,11 @@ module.exports = function () {
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            var day = this.date.split(' ')[0];
+            var time = this.date.split(' ')[1];
+            var res = pattern.replace('%DD', day);
+            res = res.replace('%HH:%MM', time);
+            return res;
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
